b830b66a79e865fac7dcf31cc5e95594008d81b6
Lateesha <Lateesha@BEATMACHINE>
2017-01-12 00:08:44 -0500

    added repeat

--- Program.cs
+++ Program.cs
@@ -10,6 +10,43 @@ namespace PigLatin1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pig Latin Translator");
+
+            int repeat = 100;
+            for (int i = 0; i < repeat; i++)
+            {
+
+                string vowel = "AEIOUaeiou";
+            string myString = Console.ReadLine();
+
+
+
+            char firstLetter = myString[0];
+            char secondLetter = myString[1];
+
+                if (vowel.Contains(firstLetter.ToString()))
+                {
+                    Console.WriteLine($"{myString}way");
+                }
+                else if (vowel.Contains(secondLetter.ToString()) && !vowel.Contains(firstLetter.ToString()))
+
+                {
+                    string smallString = myString.Substring(1);
+                    Console.WriteLine($"{smallString}{firstLetter}ay"); // mystring.Substring
+
+
+                }
+
+                else if (!vowel.Contains(firstLetter.ToString()) && !vowel.Contains(secondLetter.ToString()))
+                {
+                    string bigString = myString.Substring(2);
+                    Console.WriteLine($"{bigString}{firstLetter}{secondLetter}ay");
+                }
+
+
+            }
         }
     }
-}
+}      
+    
+
